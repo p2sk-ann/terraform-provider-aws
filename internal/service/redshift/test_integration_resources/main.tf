9ec2f345143e341f064b3d0e9a992869f86b9cae
Masaki Hirose <mhirose680@gmail.com>
2025-03-28 23:17:34 +0900

    Update main.tf

--- internal/service/redshift/test_integration_resources/main.tf
+++ internal/service/redshift/test_integration_resources/main.tf
@@ -17,8 +17,8 @@ resource "aws_redshift_integration" "sample" {
   # source_arn       = "arn:aws:dynamodb:us-west-2:490004623576:table/sample_table2"
   target_arn = "arn:aws:redshift:us-west-2:490004623576:namespace:8c12dff8-d488-4bed-8363-6ba77bd0c3a4"
   # target_arn = "arn:aws:redshift-serverless:us-west-2:490004623576:namespace/c3465720-ce4e-4d0a-9e5b-aa2ce48d683f"
-  # kms_key_id = "arn:aws:kms:us-west-2:490004623576:key/13d6f1e4-0c19-46bb-bf92-215b413be14e"
 
+  # kms_key_id = "arn:aws:kms:us-west-2:490004623576:key/13d6f1e4-0c19-46bb-bf92-215b413be14e"
   # additional_encryption_context = {
   #   "example1" : "test1",
   #   "example2" : "test2",

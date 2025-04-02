711330d098d6963a953a629ecfbbd6374dcc7516
Masaki Hirose <mhirose680@gmail.com>
2025-04-02 15:09:34 +0900

    Update main.tf

--- internal/service/redshift/test_integration_resources/main.tf
+++ internal/service/redshift/test_integration_resources/main.tf
@@ -10,38 +10,40 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-# resource "aws_redshift_integration" "sample-dynamodb" {
-#   integration_name = "hogehoge223"
-#   description      = "fugafuga223"
-#   source_arn       = "arn:aws:dynamodb:us-west-2:490004623576:table/sample_table"
-#   # source_arn = "arn:aws:dynamodb:us-west-2:490004623576:table/sample_table"
-#   target_arn = "arn:aws:redshift:us-west-2:490004623576:namespace:661deecf-6ec2-4389-b04e-27eb3a1cc189"
-#   # target_arn = "arn:aws:redshift-serverless:us-west-2:490004623576:namespace/c3465720-ce4e-4d0a-9e5b-aa2ce48d683f"
+resource "aws_redshift_integration" "sample-dynamodb" {
+  integration_name = "hogehoge2231"
+  description      = "fugafuga223"
+  source_arn       = "arn:aws:dynamodb:us-west-2:490004623576:table/sample_table"
+  # source_arn = "arn:aws:dynamodb:us-west-2:490004623576:table/sample_table"
+  target_arn = "arn:aws:redshift-serverless:us-west-2:490004623576:namespace/eacef569-7d2d-4db5-8444-2a495f911dc6"
+  # target_arn = "arn:aws:redshift-serverless:us-west-2:490004623576:namespace/006ea032-e4f6-444a-bebd-d89fabe015fd"
 
-#   # kms_key_id = "arn:aws:kms:us-west-2:490004623576:key/13d6f1e4-0c19-46bb-bf92-215b413be14e"
-#   # additional_encryption_context = {
-#   #   "example1" : "test1",
-#   #   "example2" : "test2",
-#   # }
-
-#   tags = {
-#     Environment = "dev"
-#     Owner       = "hawaii"
-#     Project     = "hey"
-#   }
-# }
-
-resource "aws_redshift_integration" "sample-s3" {
-  integration_name = "s3-integration-hoge"
-  description      = "s3-integration-fuga"
-  source_arn       = "arn:aws:s3:::sample-s3-redshift-integration"
-  # source_arn = "arn:aws:s3:::sample-s3-redshift-integration2"
-  target_arn = "arn:aws:redshift:us-west-2:490004623576:namespace:661deecf-6ec2-4389-b04e-27eb3a1cc189"
-  # target_arn = "arn:aws:redshift-serverless:us-west-2:490004623576:namespace/c2f4bc36-309b-48f2-9f7a-2092bf391df7"
+  # kms_key_id = "arn:aws:kms:us-west-2:490004623576:key/2825eb7f-7b9e-452d-b3c2-dd0136d31386"
+  # additional_encryption_context = {
+  #   "example1" : "test1",
+  #   "example2" : "test2",
+  # }
 
   tags = {
     Environment = "dev"
-    City        = "tokyo"
-    Project     = "hi"
+    Owner       = "hawaii"
+    Project     = "hey"
   }
 }
+
+# resource "aws_redshift_integration" "sample-s3" {
+#   integration_name = "s3-integration-hoge2"
+#   description      = "s3-integration-fuga"
+#   source_arn       = "arn:aws:s3:::sample-s3-redshift-integration"
+#   # source_arn = "arn:aws:s3:::sample-s3-redshift-integration2"
+#   # target_arn = "arn:aws:redshift:us-west-2:490004623576:namespace:661deecf-6ec2-4389-b04e-27eb3a1cc189"
+#   target_arn = "arn:aws:redshift-serverless:us-west-2:490004623576:namespace/c2f4bc36-309b-48f2-9f7a-2092bf391df7"
+
+#   # kms_key_id = "arn:aws:kms:us-west-2:490004623576:key/13d6f1e4-0c19-46bb-bf92-215b413be14e"
+
+#   tags = {
+#     Environment = "dev"
+#     City        = "tokyo"
+#     Project     = "hi"
+#   }
+# }
